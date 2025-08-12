a3564624a9500f530744e670571a65f6d17d5d53
Yehonatan Magen <yehonatanmagen@Yehonatans-MacBook-Air.local>
2025-08-12 18:53:07 +0300

    add shkarim

--- app/src/api/git.py
+++ app/src/api/git.py
@@ -32,21 +32,11 @@ def handle_response(response: httpx.Response) -> JSONResponse:
 class GitAPI:
     def __init__(self, base_url, token):
         headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+        self.base_url = base_url
+        self.token = token
         self.api = BaseAPI(base_url.rstrip('/'), headers=headers)
 
-    async def get_file_sha(self, path: str) -> str:
-
-        try:
-            file_sha = await self.api.get(path)
-            handle_response(file_sha)
-
-        except httpx.RequestError as e:
-            raise GitError(status_code=500, detail=f"Git request failed: {e}")
-
-        return file_sha.json()["sha"]
-
-    async def get_file_content(self, path: str) -> str:
-        # Fetch the file details from GitHub (including the base64-encoded content)
+    async def get_file(self, path: str):
         try:
             response = await self.api.get(path)
             handle_response(response)
@@ -54,24 +44,30 @@ class GitAPI:
         except httpx.RequestError as e:
             raise GitError(status_code=500, detail=f"Git request failed: {e}")
 
-        # GitHub returns the content in base64, so we need to decode it
-        content = response.json().get("content")
+        return response.json()
 
-        decoded_content = base64.b64decode(content).decode('utf-8')
-        return decoded_content
+    async def delete_file(self, path: str, commit_message: str):
+        data = await self.get_file(path)
+        sha = data["sha"]
 
-    # async def delete_file(self, path: str, commit_message: str):
-    #     try:
-    #         response = await self.api.delete(path)
-    #         handle_response(response)
-    #
-    #     except httpx.RequestError as e:
-    #         raise GitError(status_code=500, detail=f"Git request failed: {e}")
+        payload = {
+            "sha": sha,
+            "message": commit_message,
+            "branch": "main"
+        }
+
+        try:
+            response = await self.api.delete(path, json=payload)
+            handle_response(response)
+
+        except httpx.RequestError as e:
+            raise GitError(status_code=500, detail=f"Git request failed: {e}")
+
 
     async def modify_file_content(self, path, commit_message, content):
 
         try:
-            sha = await self.get_file_sha(path)
+            sha = await self.get_file(path)["sha"]
 
         except GitError as e:
             logger.debug(f"Failed to get file sha: {path}")
--- app/src/services/git.py
+++ app/src/services/git.py
@@ -1,4 +1,4 @@
-from click import command
+import base64
 
 from app.src.api.git import GitAPI
 
@@ -19,7 +19,13 @@ class Git:
         commit_message = f"Add {name} {resource}'s values file on {region=} on {namespace=}"
         await self.api.create_new_file(path, commit_message ,values)
 
-    # async def delete_values(self, region, namespace, name, resource):
-    #     path = f'/{region}/{namespace}/{name}.yaml'
-    #     commit_message = f"Delete {name} {resource}'s values file on {region=} on {namespace=}"
-    #     await self.api.
+    async def delete_values(self, region, namespace, name, resource):
+        path = f'/{region}/{namespace}/{name}.yaml'
+        commit_message = f"Delete {name} {resource}'s values file on {region=} on {namespace=}"
+        await self.api.delete_file(path, commit_message)
+
+    async def compare_values(self, region, namespace, name, values):
+        path = f'/{region}/{namespace}/{name}.yaml'
+        enc_git_values = self.api.get_file(path)["content"]
+        git_values = base64.b64decode(enc_git_values).decode("utf-8")
+        return git_values == values
